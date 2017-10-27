c9e692a4cfac81d36ca8ba37a5f6a2b85bc76006
Alex <blakesley.alex@gmail.com>
2017-10-27 00:14:56 +0000

    Added edit and update routes/pages. App is fully functional except for delete page/route.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-var express    = require("express"),
-    app        = express(),
-    mongoose   = require("mongoose"),
-    bodyParser = require("body-parser"),
-    moment     = require('moment');
+var express        = require("express"),
+    app            = express(),
+    mongoose       = require("mongoose"),
+    bodyParser     = require("body-parser"),
+    moment         = require('moment'),
+    methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/restful_blog_app", {useMongoClient: true});  //This creates that db if there isn't one already named that
 app.use(express.static("public")); //Allows us to use stylesheet from "public" directory
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     "title": String,
     "image": String,
     "body": String,
-    "created": {type: String, default: moment().format('l')} //Date.now}  //Automatically uses current date when user submits posts
+    "created": {type: String, default: moment().format('l')}, //Automatically uses current date when user submits posts
+    "detailedCreated": {type: Date, default: Date.now}  //Automatically uses current date when user submits posts
 });
 
 var Blog = mongoose.model("Blog", blogSchema);
@@ -30,7 +33,7 @@ app.get("/blogs", function(req, res){
     Blog.find({},function(err, blogs){
         if(err){
             console.log(err);
-        }else{
+        } else {
             res.render("index", {blogs:blogs})
         }
     });
@@ -46,25 +49,44 @@ app.post("/blogs", function(req, res){
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
-        }else{
+        } else{
             res.redirect("/blogs");
         }
     });
 });
     
 // SHOW
-
 app.get("/blogs/:id", function(req, res){
     Blog.findById(req.params.id, function(err, foundBlog){
         if(err){
             console.log(err)
-        }else{
+        } else{
             res.render("show", {blog:foundBlog});         
         }
     });
 });
 
 //EDIT
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//UPDATE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('server has started');
